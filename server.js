529712c708fd2fc8ca114aec50082748d60d7e0f
rubiyadav <rubiflyeis@gmail.com>
2023-06-15 16:58:47 +0530

    update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send({ msg: "Working App" });
 });
 require('./controllers/SubscriptionCronjob')
