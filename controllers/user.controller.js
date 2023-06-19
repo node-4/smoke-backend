10add56491f4a2bd3dcdbdb9608300986a270ae6
rubiyadav <rubiflyeis@gmail.com>
2023-06-19 18:01:01 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -161,3 +161,16 @@ exports.getProfile = async (req, res) => {
                 });
         }
 };
+exports.getUserById = async (req, res) => {
+        try {
+                const userId = req.params.id;
+                const user = await userSchema.findById(userId).populate("city state district")
+                if (!user) {
+                        return res.status(404).json({ message: 'User not found' });
+                }
+                return res.status(200).json({ msg: user });
+        } catch (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Internal server error' });
+        }
+};
--- route/user.js
+++ route/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const customerRouter = express.Router();
-const { createUser, verify, login, userUpdate, getUserflame, getUserFriends, socialLogin } = require('../controllers/user.controller');
+const { createUser, verify, login, userUpdate, getUserflame, getUserById,getUserFriends, socialLogin } = require('../controllers/user.controller');
 const authJwt = require('../middlewares/authJwt')
 
 customerRouter.post('/createUser', createUser);
@@ -10,6 +10,7 @@ customerRouter.post('/socialLogin', socialLogin)
 customerRouter.put('/userUpdate', authJwt.verifyToken, userUpdate)
 customerRouter.get('/getUserflame', authJwt.verifyToken, getUserflame)
 customerRouter.get('/getUserFriends', authJwt.verifyToken, getUserFriends)
+customerRouter.get('/getUserById/:id', getUserById)
 
 
 
