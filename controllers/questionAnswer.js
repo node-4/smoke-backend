b6ef3ac5ddfce822816e46abaf1e2f510664fcf3
rubiyadav <rubiflyeis@gmail.com>
2023-07-01 13:37:35 +0530

    update 01 : 37

--- controllers/questionAnswer.js
+++ controllers/questionAnswer.js
@@ -10,9 +10,18 @@ exports.getAllQuestions = async (req, res) => {
                 let month = new Date(Date.now()).getMonth() + 1;
                 let year = new Date(Date.now()).getFullYear();
                 let fullDate = (`${date}/${month}/${year}`).toString()
-                console.log(req.user._id);
-                console.log({ userID: req.user._id, questionTime: hr+6, questionDate: fullDate });
-                const questions = await questionAnswer.find({ userID: req.user._id, questionTime: hr+6, questionDate: fullDate }).populate({ path: 'question option_1 option_2 option_3 option_4 option_5 option_6 option_7 option_8 option_9 option_10 option_11 option_12', select: 'question firstName lastName userName' },);
+                let min = new Date(Date.now()).getMinutes();
+                let hrs;
+                if (min) {
+                        if (min > 30) {
+                                hrs = hr + 5
+                        } else {
+                                hrs = hr + 6
+                        }
+                }
+                console.log(hrs);
+                console.log({ userID: req.user._id, questionTime: hrs, questionDate: fullDate });
+                const questions = await questionAnswer.find({ userID: req.user._id, questionTime: hrs, questionDate: fullDate }).populate({ path: 'question option_1 option_2 option_3 option_4 option_5 option_6 option_7 option_8 option_9 option_10 option_11 option_12', select: 'question firstName lastName userName' },);
                 if (questions.length == 0) {
                         return res.status(404).json({ status: 404, message: "Question not found.", data: {} });
                 }
