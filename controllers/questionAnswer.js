0bc8040784beb31ea0dc6275a20c38833a9425c8
rubiyadav <rubiflyeis@gmail.com>
2023-07-03 12:19:23 +0530

    update 12:16

--- controllers/questionAnswer.js
+++ controllers/questionAnswer.js
@@ -24,7 +24,7 @@ exports.getAllQuestions = async (req, res) => {
                                 hr = hrs1 + 5
                         }
                 }
-                console.log({ userID: req.user._id, serverTime: hr, questionTime: hrs, questionDate: fullDate });
+                console.log({ userID: req.user._id, serverTime: hr, questionTime: hr, questionDate: fullDate });
                 const questions = await questionAnswer.find({ userID: req.user._id, questionTime: hrs, questionDate: fullDate }).populate({ path: 'question option_1 option_2 option_3 option_4 option_5 option_6 option_7 option_8 option_9 option_10 option_11 option_12', select: 'question emoji type firstName lastName userName' },);
                 if (questions.length == 0) {
                         return res.status(404).json({ status: 404, message: "Question not found.", data: {} });
