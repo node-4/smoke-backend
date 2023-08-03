1e9ff5882a1b79358bdc49bedec1055757c08c6f
rubiyadav <rubiflyeis@gmail.com>
2023-08-03 12:12:59 +0530

    create an api

--- route/ChatRouter.js
+++ route/ChatRouter.js
@@ -6,13 +6,13 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({ cloud_name: "dbrvq9uxa", api_key: "567113285751718", api_secret: "rjTsz9ksqzlDtsrlOPcTs_-QtW4", });
 const storage = new CloudinaryStorage({
-        cloudinary: cloudinary, params: { folder: "images/image", allowed_formats: ["jpg", "jpeg", "mp4", "mp3", "png", "PNG", "xlsx", "xls", "pdf", "PDF"], },
+        cloudinary: cloudinary, params: { folder: "smoke/chat", allowed_formats: ["jpg", "jpeg", "mp4", "mp3", "png", "PNG", "xlsx", "xls", "pdf", "PDF"], },
 });
 const upload = multer({ storage: storage });
 var cpUpload = upload.fields([{ name: 'video', maxCount: 1 },
- { name: 'image', maxCount: 1 },
- { name: 'docs', maxCount: 1 },
- { name: 'audio', maxCount: 1 },]);
+{ name: 'image', maxCount: 1 },
+{ name: 'docs', maxCount: 1 },
+{ name: 'audio', maxCount: 1 },]);
 
 router.post('/userChat', [authJwt.verifyToken], cpUpload, userController.userChat);
 /**
