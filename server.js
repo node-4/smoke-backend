91e111a19bb9ce7006beb9a84392f32e621ef3d6
rubiyadav <rubiflyeis@gmail.com>
2023-09-06 19:12:39 +0530

    update

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
   res.status(200).send({ msg: "Working App" });
 });
 // require('./controllers/SubscriptionCronjob')
-require('./controllers/Cronjob/QuestionCronjob')
-require('./controllers/Cronjob/QuestionoptionCondition1')
+// require('./controllers/Cronjob/QuestionCronjob')
+// require('./controllers/Cronjob/QuestionoptionCondition1')
 app.use("/api/v1/", require("./route/user"));
 app.use("/api/v1/request", require("./route/add_request"));
 app.use("/api/v1/question", require("./route/question"))
